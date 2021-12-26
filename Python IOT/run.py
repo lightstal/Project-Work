fb46d6024d3d68d3dbc553edd1f5a31f8cae0dab
Bryan Kor <50443877+lightstal-School@users.noreply.github.com>
2021-12-26 17:44:11 +0800

    SQL DB Commit

---
+++ Python IOT/run.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('home.html', var=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
