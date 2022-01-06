757e68032f04503066069d59c6928f3a336e7306
Bryan Kor <50443877+lightstal-School@users.noreply.github.com>
2022-01-06 23:12:39 +0800

    Man...

--- Testing ENV/apiget.py
+++ Testing ENV/apiget.py
@@ -5,41 +5,49 @@ import matplotlib.pyplot as plt
 import io
 import base64
 
+
 def formatDateTime(dateTime):
     a = re.sub(r'[A-Z]', ' ', dateTime).rstrip().split()
     b = a[0].split('-')
     b[0], b[1] = b[2], b[1]
     b = '-'.join(b[:2])
-    # print(b)
     return f"{b} {a[1]}"
 
 
-
 def ReadAPI():
+    # API URL
     response = requests.get("https://api.thingspeak.com/channels/1585193/feeds.json?results=2000000")
-    a = {}
+    temp = []
+    time_arr = []
     data = response.json()
     for i in data['feeds']:
         print(i['field1'], i['created_at'])
         time = formatDateTime(i['created_at'])
         print(time)
-        a.update({time: i['field1']})
-    return a
+        temp.append(i['field1'])
+        time_arr.append(time)
+    return temp, time_arr
 
 
-def main():
-    a = ReadAPI()
-    print(a)
+def plotting():
+    temp, time_arr = ReadAPI()
+    # Only select last 10 elements from the list
+    temp = temp[-10:]
+    time_arr = time_arr[-10:]
     plt.xlabel('Time')
     plt.ylabel('Temp (deg C)')
     plt.xticks(rotation=90)
     plt.subplots_adjust(bottom=0.30)
-    plt.plot(list(a.keys()), list(a.values()))
+    plt.plot(time_arr, temp, 'r-', label='Temp against Time' ) # Plot the data
     img = io.BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
     plt.show()
+
+
+def main():
+    plotting()
 
 
 main()
